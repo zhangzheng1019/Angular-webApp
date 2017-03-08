b068ac7532606edcaa44e1bc98281aba7f2768aa
zhangzheng1019 <447590461@qq.com>
2017-03-08 20:44:55 +0800

    添加了position和company模块，并进行配置

--- src/script/config/router.js
+++ src/script/config/router.js
@@ -1,17 +1,20 @@
 'use strict';
 
+
 angular.module('app', ['ui.router']).config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
-    'use strict';
     $stateProvider.state('main', {
         url: '/main',
         templateUrl: 'view/main.html',
         controller: 'mainCtrl'
+    }).state('position', {
+        url: '/position/:id',
+        templateUrl: 'view/position.html',
+        controller: 'positionCtrl'
+    }).state('company', {
+        url: '/company/:id',
+        templateUrl: 'view/company.html',
+        controller: 'companyCtrl'
     });
-    // $stateProvider.state('search', {
-    //     url: '/search',
-    //     templateUrl: 'view/search.html',
-    //     controller: 'searchCtrl'
-    // });
 
     $urlRouterProvider.otherwise('main');
 }]);
