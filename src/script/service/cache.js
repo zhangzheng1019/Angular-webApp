89def7fc1d3fcf99d7d64e262f83287d7b4e0ea8
zhangzheng1019 <447590461@qq.com>
2017-03-11 14:49:44 +0800

    编写服务

---
+++ src/script/service/cache.js
@@ -0,0 +1,27 @@
+'use strict';
+angular.module('app')
+    .service('cache', ['$cookies', function($cookies) {
+        this.put = function(key, value) {
+            return $cookies.put(key, value);
+        };
+        this.get = function(key) {
+            return $cookies.get(key);
+        };
+        this.remove = function(key) {
+            $cookies.remove(key);
+        };
+    }]);
+
+// .factory('cache', ['$cookies', function($cookies) {
+//     return {
+//         get: function(key) {
+//             return $cookies.get(key);
+//         },
+//         put: function(key, value) {
+//             return $cookies.put(key, value);
+//         },
+//         remove: function(key) {
+//             $cookies.remove(key);
+//         }
+//     }
+// }]);
