5008b4e40c70d9e20f3c0f7373ed6d09bbe9e45d
zhangzheng1019 <447590461@qq.com>
2017-03-21 08:43:52 +0800

    各模块controller

--- src/script/controller/favoriteCtrl.js
+++ src/script/controller/favoriteCtrl.js
@@ -1,5 +1,7 @@
 'use strict';
 
 angular.module('app').controller('favoriteCtrl', ['$scope', '$http', function($scope, $http) {
-
+    $http.get('data/myFavorite.json').then(function(resp) {
+        $scope.list = resp.data;
+    });
 }])
--- src/script/controller/postCtrl.js
+++ src/script/controller/postCtrl.js
@@ -1,14 +1,32 @@
 'use strict';
-
-angular.module('app').controller('postCtrl', ['$scope', '$http', function($scope, $http) {
+angular.module('app').controller('postCtrl', ['$http', '$scope', function($http, $scope) {
     $scope.tabList = [{
         id: 'all',
-        name: "全部"
+        name: '全部'
     }, {
         id: 'pass',
-        name: "面试邀请"
+        name: '面试邀请'
     }, {
         id: 'fail',
-        name: "不合适"
-    }]
-}])
+        name: '不合适'
+    }];
+    $http.get('data/myPost.json').then(function(res) {
+        $scope.positionList = res.data;
+    });
+    $scope.filterObj = {};
+    $scope.tClick = function(id, name) {
+        switch (id) {
+            case 'all':
+                delete $scope.filterObj.state;
+                break;
+            case 'pass':
+                $scope.filterObj.state = '1';
+                break;
+            case 'fail':
+                $scope.filterObj.state = '-1';
+                break;
+            default:
+
+        }
+    }
+}]);
