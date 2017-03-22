ebcb88fcabf24afd1d94ba0b76b74e5791865e74
zhangzheng1019 <447590461@qq.com>
2017-03-22 19:15:11 +0800

    添加cache自定义服务

--- src/script/service/cache.js
+++ src/script/service/cache.js
@@ -1,17 +1,15 @@
 'use strict';
-angular.module('app')
-    .service('cache', ['$cookies', function($cookies) {
-        this.put = function(key, value) {
-            return $cookies.put(key, value);
-        };
-        this.get = function(key) {
-            return $cookies.get(key);
-        };
-        this.remove = function(key) {
-            $cookies.remove(key);
-        };
-    }]);
-
+angular.module('app').service('cache', ['$cookies', function($cookies) {
+    this.put = function(key, value) {
+        $cookies.put(key, value);
+    };
+    this.get = function(key) {
+        return $cookies.get(key);
+    };
+    this.remove = function(key) {
+        $cookies.remove(key);
+    };
+}]);
 // .factory('cache', ['$cookies', function($cookies) {
 //     return {
 //         get: function(key) {
